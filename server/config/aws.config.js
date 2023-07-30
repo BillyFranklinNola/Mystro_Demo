05745934269c137dfd6d7e00abd738ed9ba095ac
Billy Franklin <120335506+BillyFranklinNola@users.noreply.github.com>
2023-07-29 22:27:25 -0500

    testing aws variables

--- server/config/aws.config.js
+++ server/config/aws.config.js
@@ -11,15 +11,19 @@ async function getParameterValue(parameterName) {
     };
 
     const response = await ssm.getParameter(params).promise();
+    console.log(`Retrieved ${parameterName} from AWS SSM Parameter Store.`);
     return response.Parameter.Value;
 }
 
 async function fetchEnvironmentVariables() {
     const secretKey = await getParameterValue('SECRET_KEY');
+    console.log(secretKey);
     process.env.API_KEY = secretKey;
     const mapsAPI = await getParameterValue('REACT_APP_GOOGLE_MAPS_API_KEY');
+    console.log(mapsAPI);
     process.env.REACT_APP_GOOGLE_MAPS_API_KEY = mapsAPI;
     const nodeEnv = await getParameterValue('NODE_ENV');
+    console.log(nodeEnv);
     process.env.NODE_ENV = nodeEnv;
 }
 
