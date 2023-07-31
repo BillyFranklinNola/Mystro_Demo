40dfe96423e54a0dd059d6fc2783395eaa57aa4e
Billy Franklin <120335506+BillyFranklinNola@users.noreply.github.com>
2023-07-30 23:54:51 -0500

    heroku

--- server/config/mongoose.config.js
+++ server/config/mongoose.config.js
@@ -4,4 +4,4 @@ console.log(process.env.MONGO_URI)
 
 mongoose.connect(process.env.MONGO_URI)
     .then(() => console.log('Established a connection to the database'))
-    .catch(err => console.log('Something went wrong when connecting to the database ', err));
+    .catch(err => console.log('Something went wrong when connecting to the databaseheroku logs', err));
