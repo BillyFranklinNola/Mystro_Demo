e3fe51ee7a7776d5dcc6060596a0b5d7d078bbb8
Billy Franklin <120335506+BillyFranklinNola@users.noreply.github.com>
2023-07-30 23:24:08 -0500

    heroku

--- server/config/mongoose.config.js
+++ server/config/mongoose.config.js
@@ -1,8 +1,5 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('process.env.MONGO_URI', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-})
+mongoose.connect('process.env.MONGO_URI')
     .then(() => console.log('Established a connection to the database'))
     .catch(err => console.log('Something went wrong when connecting to the database ', err));
