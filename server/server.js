20978263cfde641b37994ec93893984b8bc15d12
Billy Franklin <120335506+BillyFranklinNola@users.noreply.github.com>
2023-08-04 22:08:33 -0500

    cleaning up

--- server/server.js
+++ server/server.js
@@ -31,21 +31,4 @@ if(process.env.NODE_ENV === 'production') {
 // -----------------------deployment-----------------------
 
 
-
-// async function startServer() {
-//     try {
-//         await fetchEnvironmentVariables();
-//         if (process.env.NODE_ENV === 'production') {
-//             console.log('Running in production environment.');
-//             } else {
-//             console.log('Running in development environment.');
-//         }
-//         app.listen(port, () => console.log(`Listening on port: ${port}`));
-//     } catch (error) {
-//         console.error('Error fetching environment variables:', error);
-//     }
-// }
-
-// startServer();
-
 app.listen(port, () => console.log(`Listening on port: ${port}`) );
